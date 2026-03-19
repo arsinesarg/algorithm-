d3003d375dae6e80c6563942bab58aa252dfc1e0
arsinesarg <arsineip@mail.ru>
2026-03-20 00:23:30 +0400

    Time: 0 ms (100.00%) | Memory: 52.3 MB (82.28%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    bool canJump(std::vector<int>& nums) {
+        int max_reach = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; ++i) {
+            if (i > max_reach) {
+                return false;
+            }
+            
+            max_reach = std::max(max_reach, i + nums[i]);
+            
+            if (max_reach >= n - 1) {
+                return true;
+            }
+        }
+        
+        return true;
+    }
+};
