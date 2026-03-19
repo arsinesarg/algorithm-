60c3e097a3a1f6014a5a01ab124bd4b70fcc2877
arsinesarg <arsineip@mail.ru>
2026-03-20 00:21:53 +0400

    Time: 4 ms (14.46%) | Memory: 15.4 MB (81.58%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int minPathSum(std::vector<std::vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        std::vector<int> dp(n, grid[0][0]);
+
+        for (int j = 1; j < n; ++j) {
+            dp[j] = dp[j - 1] + grid[0][j];
+        }
+
+        for (int i = 1; i < m; ++i) {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < n; ++j) {
+                dp[j] = std::min(dp[j], dp[j - 1]) + grid[i][j];
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
