0b9cfcf56a9139981043798026745c465ace519d
Арсине Саргсян <arsineip@mail.ru>
2026-05-24 16:31:48 +0400

    Time: 0 ms (100.00%) | Memory: 101.9 MB (98.05%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(), res = 0;
+        for (int r = 0; r < m; r++)
+            for (int c = 0; c < n; c++)
+                if (grid[r][c] == 1) {
+                    res += 4;
+                    if (r > 0 && grid[r-1][c]) res -= 2;
+                    if (c > 0 && grid[r][c-1]) res -= 2;
+                }
+        return res;
+    }
+};
